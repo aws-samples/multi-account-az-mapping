16863dc6e9624c4dc104efa370692eb40819c6aa
Adam Spicer <adspicer@amazon.com>
2021-08-27 14:34:02 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
-## My Project
+## Mutli-Account Availability Zone (AZ) mapping
 
-TODO: Fill this README out!
+AWS independently maps Availability Zone names for each individual account. This means that for example an AZ named us-east-1a in one account might not be the same location as us-east-1a in another AWS account. 
 
-Be sure to:
+Multi-account organizations may have a requirement to ensure a consistent AZ mapping for all of their subnets in their VPCs across their accounts. This solution can deploy resources in each of your accounts to aide in ensuring the same Availability Zone IDs are used when creating subnets, and not relying on the AZ name.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+### How it works
+The `az-mapping.yaml` deploys a set of Systems Manager Parameter Store values storing the Availabilty Zone Id and Availability Zone Name. These values are then used when VPCs are created to ensure a consistent mapping to Zone Name from a set of given Zone Ids in each account.
+
+1. Determine the Availability Zone Ids for the AZs you want to use.
+2. Use the az-mapping.yaml to create a stack in all relevant accounts. (Customizations for Control Tower Solution, CloudFormation StackSets, etc.)
+   * Specify the Zone Ids parameter value, comma separated, and in an ordered format.
+3. Update your infrastructure as code that creates your VPC to reference the SSM Parameter Store values to get the AZ Name for each subnet.
+4. Create VPCs in your relevant accounts using your infrastructure as code.
+
+See `vpc-example.yaml` for a working CloudFormation example which creates a VPC using the SSM Parameter Store values for the AZ Names.
+
 
 ## Security
 
---
+++ src/az-mapping.py
@@ -0,0 +1,74 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import boto3
+import cfnresponse
+
+def lambda_handler(event, context):
+    azIds = event['ResourceProperties']['azIds']
+
+    if event['RequestType'] == 'Create':    
+        create(azIds)
+    elif event['RequestType'] == 'Delete':
+        delete(azIds)
+    elif event['RequestType'] == 'Update':
+        delete(azIds)
+        create(azIds)
+    else:
+        print('no changes')
+
+    response_data = {}
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, response_data)
+    
+
+# Use the ordered parameter list of azIds (zone ids) to create SSM Parameters 
+# for the AZ Mapping. Store the resulting zone name mapping along with the zone-id
+def create(azIds):
+    ssm = boto3.client('ssm')
+    
+    azs = getAZs(azIds)
+    for az in azs:
+        azId = az.get('ZoneId')
+        azName = az.get('ZoneName')
+        # AZ Number should match the ordered zone id parameter
+        azNumber = azIds.index(azId) + 1
+        ssm.put_parameter(
+            Name='/az-mapping/az' + str(azNumber), 
+            Description=azId,
+            Value=azName,
+            Type='String',
+            Overwrite=True,
+            Tier='Standard')
+
+# Delete the az mapping parameters
+def delete(azIds):
+    ssm = boto3.client('ssm')
+    
+    azNumber=1
+    azs = getAZs(azIds)
+    for az in azs:
+        ssm.delete_parameter(
+            Name='/az-mapping/az' + str(azNumber)
+            )
+        azNumber = azNumber + 1
+
+# Get the AZ objects that match the given zone IDs
+def getAZs(azIds):
+    ec2c = boto3.client('ec2')
+    r = ec2c.describe_availability_zones(
+            ZoneIds=azIds
+        )
+    azs = r.get('AvailabilityZones')
+    return azs
